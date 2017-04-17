558ae4e9b8d859e115d1d84740741b687de818bb
Dylan Frankland <dylan@frankland.io>
2017-04-16 22:09:14 -0700

    Add rollup

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+
+export default {
+  entry: './lib/index.js',
+  format: 'cjs',
+  external: ['noble'],
+  plugins: [
+    nodeResolve(),
+    babel({
+      presets: [
+        [
+          'env',
+          {
+            modules: false,
+            targets: {
+              node: 6,
+            },
+          },
+        ],
+        'stage-0',
+      ],
+      babelrc: false,
+    }),
+  ],
+  dest: './dist/index.js',
+};
